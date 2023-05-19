9edc0f4f2aef18ed8f85c622be61b7936c5cc4b0
khiemfe <128385190+khiemfe@users.noreply.github.com>
2023-05-19 16:05:55 +0700

    first commit

--- access/main.js
+++ access/main.js
@@ -1,3 +1,30 @@
+// Clock-number
+setInterval(()=>{
+    const time = document.getElementById("time");
+    let date = new Date();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+    let day_night = "AM";
+
+    if(hours > 12){
+        hours = hours - 12;
+        day_night = "PM";
+    }
+    if(hours < 10){
+        hours = "0" + hours;
+    }
+    if(minutes < 10){
+        minutes = "0" + minutes;
+    }
+    if(seconds < 10){
+        seconds = "0" + seconds;
+    }
+
+    time.textContent = hours + ":" + minutes + ":" + seconds + " " + day_night;
+});
+
+
 //container
 const $ = document.querySelector.bind(document)
 const $$ = document.querySelectorAll.bind(document)
@@ -60,21 +87,39 @@ sayings_right.onclick = function() {
     sayings_lines[indexcurent].classList.add('active')
 }
 
+
 //Phần sở thích
 const hobbies = $$('.container_right-body-hobbies-part-title')
 const hobbies_active = $('.container_right-body-hobbies-part-title.active')
 const hobbies_line = $('.container_right-body-hobbies-part-line')
 const hobbies_items = $$('.container_right-body-hobbies-item')
 
-hobbies_line.style.left = hobbies_active.offsetLeft + 'px'
-hobbies_line.style.width = hobbies_active.offsetWidth + 'px'
-console.log(hobbies_active.offsetWidth)
+var hobbies_modal = $('.container_left-self.hoobbies')
+
+//khi click ra chỗ khác (Bản thân, Học Vấn, Mục tiêu) thì khi click lại vào Sở thích 
+// nó sẽ reset lại về ô bóng đá
+hobbies_modal.addEventListener('click', function() {
+    hobbies_line.style.left = hobbies_active.offsetLeft + 'px'
+    hobbies_line.style.width = hobbies_active.offsetWidth + 'px'
+
+    $('.container_right-body-hobbies-item.active').classList.remove('active')
+    $('.container_right-body-hobbies-item:first-child').classList.add('active')
+})
+
+//khi click ra chỗ khác (Bản thân, Học Vấn, Mục tiêu) thì khi click lại vào Sở thích 
+// nó sẽ giữ nguyên như lúc nảy
+// hobbies_modal.addEventListener('click', function() {
+//     hobbies_line.style.left = $('.container_right-body-hobbies-part-title.active').offsetLeft + 'px'
+//     hobbies_line.style.width = $('.container_right-body-hobbies-part-title.active').offsetWidth + 'px'
+// })
 
 hobbies.forEach((hob, index) => {
     const hobbies_item = hobbies_items[index]
     hob.onclick = function() {
         $('.container_right-body-hobbies-item.active').classList.remove('active')
 
+        hobbies_active.classList.remove('active')
+        hob.classList.add('active')
         hobbies_line.style.left = this.offsetLeft + 'px' 
         hobbies_line.style.width = this.offsetWidth + 'px' 
 
@@ -82,13 +127,15 @@ hobbies.forEach((hob, index) => {
     }
 })
 
-//cầu thủ yêu thíc
+
+
+//cầu thủ yêu thích
 
 var icon_left = $('.container_right-body-hobbies-football-player-icon.left')
 var icon_right = $('.container_right-body-hobbies-football-player-icon.right')
 
 var player = $('.container_right-body-hobbies-football-player')
-console.log(player)
+// console.log(player)
 
 // var count = 0
 // icon_right.onclick = function () {
@@ -108,12 +155,14 @@ console.log(player)
 //     }
 // }
 
+//khi click vào ô Sở Thích thì nó sẽ trở lại vị trí ban đầu
+hobbies_modal.addEventListener('click', function() {
+    player.scrollTo(0, 1);
+})
 
 player.addEventListener('scroll', function() {
     if(player.scrollLeft > 0) {
-        console.log(player.scrollLeft)
         icon_left.style.display = 'block'
-
     }
     if (player.scrollLeft <= 1) {
         icon_left.style.display = 'none'
@@ -124,12 +173,9 @@ player.addEventListener('scroll', function() {
     }
 })
 
-var scrollWidth
+// var scrollWidth
 icon_right.onclick = function () {
     player.scrollBy(300, 0);
-    player.scrollBy({
-        behavior: "smooth"
-    });
     icon_left.style.display = 'block'
 
     console.log(player.scrollLeft)
@@ -143,7 +189,7 @@ icon_right.onclick = function () {
     }
 }
 
-console.log(player.scrollWidth)
+// console.log(player.scrollWidth)
 
 
 
